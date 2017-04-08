3a8f15c73144a3be4c7772b17784531b5cbaea6d
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-08 00:04:54 -0300

    Adding error_checks, renaming variables

--- unscramble_words.py
+++ unscramble_words.py
@@ -2,25 +2,35 @@
 
 from __future__ import print_function
 import sys, argparse, re
+from os.path import isfile
+from numpy import random
 
-def process_text(text_file):
-    """Process input text file and return a list of unique words sorted by descending word length"""
+def process_text(dict_file):
+    """Process 'dict_file' and return a list of unique words sorted by descending word length"""
 
-    text = text_file.read().lower()
+    text = dict_file.lower()
     text = re.sub(r"[^a-zA-Z ]", "", text)
     unique_words = list(set(text.split(" ")))
     unique_words.sort(key=lambda word: len(word), reverse=True)
 
     return unique_words
 
-def unscramble(scrambled_file, word_vocab):
-    """Unscramble scrambled_file using word_vocab. Words that weren't matched will be written in uppercase."""
+def drop_words(word_vocab, n=3):
+    """Drop n words from 'word_vocab' to simulate unknown words."""
 
-    scrambled = scrambled_file.read().upper()
+    dropped_words = random.choice(word_vocab, n)
+    dropped_vocab = [w for w in word_vocab if w not in dropped_words]
+    return dropped_vocab, dropped_words
+
+
+def unscramble(scrambled_file, word_vocab):
+    """Unscramble 'scrambled_file' using 'word_vocab'. Words that weren't matched will be written in uppercase."""
+
+    scrambled = scrambled_file.upper()
     for word in word_vocab:
         scrambled.replace(word.upper(), " " + word + " ")
     
-    unscrambled = scrambled.trim()
+    unscrambled = scrambled.strip()
 
     return unscrambled
 
@@ -29,39 +39,87 @@ def accuracy(text):
     return sum(1 for c in text if c.isupper())
 
 def main(argv):
-    # Parsing inputs
-    parser = argparse.ArgumentParser(
-        description="Unscramble words from a string based on a given dictionary.")
 
-    parser.add_argument("text_file", metavar="text_file", type=file, 
+    # Configuring arguments    
+    parser = argparse.ArgumentParser(
+        description=(
+            "Unscramble words from a string based on a given dictionary. Words not present on "
+            "the dictionary will be written in UPPERCASE on the output and will count towards "
+            "the number of unrecognized characters." 
+        )
+    )
+
+    parser.add_argument("dict_file", metavar="dict_file", type=str, 
         help="A text file containing all words that should be considered for unscrambling.")
 
-    parser.add_argument("scrambled_file", metavar="scrambled_file", type=argparse.FileType('w'), 
+    parser.add_argument("scrambled_file", metavar="scrambled_file", type=str, 
         help="File containing the string with all words scrambled")
 
-    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Verbose output", required=False)
+    parser.add_argument("-d", "--drop", dest="drop", metavar="n", type=int, default=3,
+        help="Number of random words to drop from dictionary, before scrambling")
+
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False, 
+        help="Verbose output", required=False)
 
     try:
+        # Parsing arguments
         args = parser.parse_args()
-        text_file = args.text_file
-        scrambled_file = args.scrambled_file
+
+        # Optional arguments
         verbose = args.verbose
+        drop = args.drop
 
-        word_vocab = process_text(text_file)
-        unscrambled_text = unscramble(scrambled_file, word_vocab)
+        # Checking if input files exist
+        if not isfile(args.dict_file):
+            print("Invalid argument: dict_file == " + args.dict_file)
+            return
+        
+        if not isfile(args.scrambled_file):
+            print("Invalid argument: scrambled_file == " + args.scrambled_file)
+            return
+        
+        # Reading files
+        dict_file_text = ""
+        with open(args.dict_file) as f:
+            dict_file_text = f.read()
 
+        # Error handling
+        if len(dict_file_text.strip()) == 0:
+            print("dict_file is empty.")
+        
+        scrambled_file_text = ""
+        with open(args.scrambled_file) as f:
+            scrambled_file_text = f.read()
+
+        if len(scrambled_file_text.strip()) == 0:
+            print("scrambled_file is empty.")
+              
+        # Creating word vocabulary
+        word_vocab = process_text(dict_file_text)
+
+        # Optionally, removing words from dictionary 
+        if drop > 0:
+            word_vocab, dropped_words = drop_words(word_vocab, n=drop)
+
+        # Unscrambling input
+        unscrambled_text = unscramble(scrambled_file_text, word_vocab)
+
+        # Verbose output
         if verbose:
-            print("Scrambled text:", scrambled_file.read())
-            print("Dictionary: ", word_vocab)
+            print("Scrambled text:", scrambled_file_text)
+            print("Dictionary:", word_vocab)
+            print("Words dropped from Dictionary:", dropped_words)
 
+        # Final output
         print("Unscrambled text:", unscrambled_text)
         print("Unrecognized characters: ", accuracy(unscrambled_text))
 
     except IOError as err:
-        print(err.strerror, "->", err.filename)
+        print(err.filename, err.message)
+    except ValueError as err:
+        print(err.message)
     except:
-        err = sys.exc_info()
-        print("Unexpected error:", err[1])
+        print("Unexpected error")
 
 
 
