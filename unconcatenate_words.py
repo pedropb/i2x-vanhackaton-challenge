9c332df0b7717227e48c766c4197cdce8f8a2076
Pedro Baracho <pedropbaracho@gmail.com>
2017-04-08 13:33:29 -0300

    Refactored the whole solution.
    Using text8 dataset as a huge corpus of vocabulary from wikipedia.
    Reworked parameters: now the dictionary is fixed.
    New parameter frequency-threshold

--- unconcatenate_words.py
+++ unconcatenate_words.py
@@ -1,23 +1,97 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-import sys, argparse, re
-from os.path import isfile
+import dataset_tools
+import sys, argparse, os
+from collections import Counter
+import pickle
 
-def process_text(dict_file):
-    """Process 'dict_file' and return a list of unique words sorted by descending word length"""
+def process_dict(text, frequency_threshold):
+    """
+    Process 'text' and return a list of unique words sorted by descending word length.
+    During processing, we remove single letters words (1 char). We also remove uncommon 
+    words (<1% occurrence).
 
-    # text = dict_file.lower()
-    # text = text.replace(" ", "")
-    # text = text.replace("\n", " ")
-    # text = re.sub(r"[^a-zA-Z ]", "", text)
-    unique_words = [w for w in set(text.split(" ")) if len(w) > 1 and len(w) < 20]
+    Our dictionary is composed by the text8 dataset (http://mattmahoney.net/dc/textdata).
+
+    This dataset contains the first 10^9 bytes of the English Wikipedia dump on Mar. 3, 2006.
+
+    This dataset is cleaned and only contains lowercase characters from a to z, plus whitespace.
+    """
+
+    # Trying to load previous unique_words (pickle file)
+    UNIQUE_WORDS_PICKLE = "unique_words_with_frequency_" + str(frequency_threshold) + ".pickle"
+    
+    unique_words = None
+    if os.path.isfile(UNIQUE_WORDS_PICKLE):
+        try:
+            with open(UNIQUE_WORDS_PICKLE, 'r') as f:
+                unique_words = pickle.load(f)
+        except:
+            os.remove(UNIQUE_WORDS_PICKLE)
+            unique_words = None
+
+    if (type(unique_words) == list):
+        return unique_words
+
+
+    WORD_COUNT_PICKLE = "word_count.pickle"
+    WORD_COUNT = 253855
+
+    print("Processing dictionary. This will take a while.")
+
+    # Trying to load previous word_count (pickle file)
+    word_count = None
+    if os.path.isfile(WORD_COUNT_PICKLE):
+        try:
+            with open(WORD_COUNT_PICKLE, 'r') as f:
+                word_count = pickle.load(f)
+            if len(word_count) != WORD_COUNT:
+                os.remove(WORD_COUNT_PICKLE)
+                word_count = None
+        except:
+            raise
+            os.remove(WORD_COUNT_PICKLE)
+            word_count = None
+
+    # count words
+    if word_count == None:
+        print("Pickle file not found. Counting word occurence...")
+
+        # grab all the words
+        words = text.split(" ")
+
+        # counting word occurence
+        word_count = dict(Counter(words).most_common())
+    
+        # saving word count for future reuse
+        with open(WORD_COUNT_PICKLE, 'w') as f:
+            pickle.dump(word_count, f)
+            print("Word count saved for future reuse.")
+    
+    # making sure we have the correct count loaded
+    assert(type(word_count) == dict)
+    assert(len(word_count) == WORD_COUNT)
+
+    # remove the duplicates and single-character words.
+    unique_words = [w for w in word_count.keys() if len(w) > 1]
+    vocab_size = len(unique_words)
+    print("Vocab size:", vocab_size)
+
+    # remove words with frequency lower than 1%
+    unique_words = [word for word in unique_words if float(word_count[word]) / vocab_size > frequency_threshold]
+    print("Vocab size (>%.3f%% frequency): %d" % ((frequency_threshold * 100), len(unique_words)))
+
     unique_words.sort(key=lambda word: len(word), reverse=True)
-    unique_words.append('a', 'i')
+    unique_words.append('a')
+    unique_words.append('i')
+
+    # save unique words for future reuse
+    with open(UNIQUE_WORDS_PICKLE, 'w') as f:
+        pickle.dump(unique_words, f)
+        print("unique_words saved for future reuse.")
 
     return unique_words
-
-
 
 def unconcatenate(concatenated_file, word_vocab):
     """Unconcatenate 'concatenated_file' using 'word_vocab'. Words that weren't matched will be written in uppercase."""
@@ -35,7 +109,6 @@ def accuracy(text):
     return sum(1 for c in text if c.isupper())
 
 def main(argv):
-
     # Configuring arguments    
     parser = argparse.ArgumentParser(
         description=(
@@ -49,34 +122,40 @@ def main(argv):
     parser.add_argument("concatenated_file", metavar="concatenated_file", type=str, 
         help="File containing the string with all words concatenated")
 
-    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False, 
-        help="Verbose output", required=False)
+    parser.add_argument("-t", "--frequency-threshold", dest="frequency_threshold", type=float, default=0.001, 
+        help=(
+            "Frequency threshold (in %) for considering words from the corpus dictionary (Default: 0.001)."
+            " For example, words that appear on the corpus dictionary less than 0.001% won't be used."
+        ), required=False)
 
     # Parsing arguments
     args = parser.parse_args()
 
     # Print help and exit if there are not enough arguments
-    if len(argv) < 2:
+    if len(argv) < 1:
         parser.print_help()
         sys.exit(1)
 
     try:
-        # Optional arguments
-        verbose = args.verbose
+        # Threshold argument
+        threshold = args.frequency_threshold / 100
+        if (threshold > 1 or threshold <= 0):
+            print("Invalid argument: frequency threshold. Expected float between 100 and 0 (not inclusive).")
+
+        # Download DICT FILE
+        dict_file = dataset_tools.maybe_download()
 
         # Checking if input files exist
-        if not isfile(args.dict_file):
-            print("Invalid argument: dict_file == " + args.dict_file)
+        if not os.path.isfile(dict_file):
+            print("Dictionary file missing: " + dict_file)
             return
         
-        if not isfile(args.concatenated_file):
+        if not os.path.isfile(args.concatenated_file):
             print("Invalid argument: concatenated_file == " + args.concatenated_file)
             return
         
         # Reading files
-        dict_file_text = ""
-        with open(args.dict_file) as f:
-            dict_file_text = f.read()
+        dict_file_text = dataset_tools.read_dict_file(dict_file)
 
         # Error handling
         if len(dict_file_text.strip()) == 0:
@@ -90,17 +169,13 @@ def main(argv):
             print("concatenated_file is empty.")
               
         # Creating word vocabulary
-        word_vocab = process_text(dict_file_text)
+        word_vocab = process_dict(dict_file_text, frequency_threshold=threshold)
 
         # Unconcatenating input
         unconcatenated_text = unconcatenate(concatenated_file_text, word_vocab)
 
-        # Verbose output
-        if verbose:
-            print("Concatenated text:", concatenated_file_text[:100])
-            print("Dictionary:", word_vocab[:20])
-
         # Final output
+        print("Concatenated text:", concatenated_file_text)
         print("Unconcatenated text:", unconcatenated_text)
         print("Unrecognized characters: ", accuracy(unconcatenated_text))
 
@@ -109,6 +184,7 @@ def main(argv):
     except ValueError as err:
         print(err.message)
     except:
+        raise
         print("Unexpected error")
 
 
